229035e3571a8a6fc293343ebb984f4a357f9af7
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-03-06 08:58:35 -0300

    Implementação da Arithmetic Operations

---
+++ src/CSharp.Fundamentals.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CSharp.Fundamentals.Application.DTOs;
+using CSharp.Fundamentals.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CSharp.Fundamentals.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ArithmeticOperation, ArithmeticOperationDto>();
+        }
+    }
+}
